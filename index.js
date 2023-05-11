6cd16af1c39beba23519001d2bd1a5358d6c279a
kanak shukla <kanak@optimalvirtualemployee.com>
2023-05-11 10:33:40 +0530

    routes added

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello Kanak !! Welcomw to Node with Azure services")
+    res.send("Hello Kanak !! Welcome to Node with Azure services")
+})
+
+app.get("/home", (req, res) => {
+    res.send("Hello Kanak !! Welcome to Azure")
 })
 
 app.listen(port, () => {
